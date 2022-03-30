4faa20a1c244e6c66adb374b347246cc3b1de3a6
Cherrelle-Ward <Cherrelle_Ward@hotmail.co.uk>
2022-03-30 21:15:07 +0100

    crud complete

--- index.js
+++ index.js
@@ -12,18 +12,61 @@ const mongoose = require("mongoose");
   const Cat = mongoose.model("Cat", {
     //   unique is a restraint
     name: { type: String, unique: true },
+    age: {
+      type: Number,
+      min: 1,
+      max: 100,
+      validate: {
+        validator: (num) => num % 2 === 0,
+        message: (props) => `${props.value} is not even`,
+      },
+    },
   });
 
-  //   delete method
+  //!   delete method
   //   await Cat.deleteMany({});
 
-  const cat = new Cat({ name: "billy" });
-  await cat.save();
-  console.log("im the cat", cat);
+  // ! how to add a cat object
+  // try {
+  //   const cat = new Cat({ name: "dan", age: 2 });
+  //   await cat.save();
+  //   console.log("im the cat", cat);
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
-  const foundCat = await Cat.findById("624436cd0f59c7823d60c298");
-  console.log("im foundCat", foundCat);
+  //! how to find by ID
+  // const foundCat = await Cat.findById("624436cd0f59c7823d60c298");
+  // console.log("im foundCat", foundCat);
+  ////////////////////////////
+  //! how to update object
+  // todo add errors
+  // try {
+  //   await Cat.updateOne({ name: "dylan" }, { name: "Robin", age: 20 });
+  // } catch (error) {
+  //   console.log(`error name does not exist ${error}`);
+  // }
+
+  // todo another example - Not working
+  // await Cat.findOneAndUpdate(
+  //   { name: "dan" },
+  //   { name: "danny" },
+  //   (error, data) => {
+  //     if (error) {
+  //       console.log(error);
+  //     } else {
+  //       console.log(data);
+  //     }
+  //   }
+  // );
+  ///////////////////////////
+  // ! LIST
+  const catList = await Cat.find({ Cat });
+  console.log(catList, "i am the catlist");
+  /////////////////////////////
 
   // terminates connection to allow us to write in terminal
   mongoose.connection.close();
 })();
+
+// todo feature / implemented list branch name for next git push
