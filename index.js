930d4baa73752d2fa36dc4c475acd5b5a568a844
Cherrelle-Ward <Cherrelle_Ward@hotmail.co.uk>
2022-03-31 14:25:38 +0100

    edited model objects

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // uniform resource identifier -URI, service on internet
 // uniform resource location (URL), is a type of URI
-
 require("dotenv").config();
+
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv));
 
 const mongoose = require("mongoose");
 
@@ -9,18 +12,18 @@ const mongoose = require("mongoose");
   // database connection linked
   mongoose.connect(process.env.MONGO_URI);
 
-  const Cat = mongoose.model("Cat", {
+  const Movies = mongoose.model("Movies", {
     //   unique is a restraint
-    name: { type: String, unique: true },
-    age: {
+    Title: { type: String, unique: true },
+    Actor: { type: String, unique: false },
+    Year: Number,
+    Genre: String,
+    Rating: {
       type: Number,
       min: 1,
-      max: 100,
-      validate: {
-        validator: (num) => num % 2 === 0,
-        message: (props) => `${props.value} is not even`,
-      },
+      max: 5,
     },
+    Director: String,
   });
 
   //!   delete method
@@ -35,6 +38,9 @@ const mongoose = require("mongoose");
   //   console.log(error);
   // }
 
+  // node index.js --name "fluffy"
+  console.log(argv.Title);
+  const movie = new Movies({ Title: argv.Title });
   //! how to find by ID
   // const foundCat = await Cat.findById("624436cd0f59c7823d60c298");
   // console.log("im foundCat", foundCat);
@@ -61,8 +67,8 @@ const mongoose = require("mongoose");
   // );
   ///////////////////////////
   // ! LIST
-  const catList = await Cat.find({ Cat });
-  console.log(catList, "i am the catlist");
+  // const catList = await Cat.find({ Cat });
+  // console.log(catList, "i am the catlist");
   /////////////////////////////
 
   // terminates connection to allow us to write in terminal
